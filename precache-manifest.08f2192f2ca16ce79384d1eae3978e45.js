de31d8320fabb71cf20aa3e538b9bae6f0a861a9
Shubhangi Garg <gshubhangi0507@gmail.com>
2022-01-26 17:00:15 +0530

    Updates

---
+++ precache-manifest.08f2192f2ca16ce79384d1eae3978e45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b811db49a519b3a801818ece9e6f3e",
+    "url": "//Portfolio/index.html"
+  },
+  {
+    "revision": "590368df2cea03008939",
+    "url": "//Portfolio/static/css/2.94b5edfc.chunk.css"
+  },
+  {
+    "revision": "16db0dac53129ebdb2c8",
+    "url": "//Portfolio/static/css/main.04406670.chunk.css"
+  },
+  {
+    "revision": "590368df2cea03008939",
+    "url": "//Portfolio/static/js/2.82a46d16.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "//Portfolio/static/js/2.82a46d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb2192ff2e60a12e45a",
+    "url": "//Portfolio/static/js/3.26a12018.chunk.js"
+  },
+  {
+    "revision": "16db0dac53129ebdb2c8",
+    "url": "//Portfolio/static/js/main.b8e0ef4a.chunk.js"
+  },
+  {
+    "revision": "38f6e094aed16f811cac",
+    "url": "//Portfolio/static/js/runtime-main.426e8ae9.js"
+  },
+  {
+    "revision": "0aa79e39b74e18c3f465d80e379e408e",
+    "url": "//Portfolio/static/media/ShuGARG-removebg-preview.0aa79e39.png"
+  },
+  {
+    "revision": "c63edc564502ffde74888a09d7cb1a1a",
+    "url": "//Portfolio/static/media/bhumi.c63edc56.jpg"
+  },
+  {
+    "revision": "7631d75b50580020f12892ce4a2408c1",
+    "url": "//Portfolio/static/media/miracle.7631d75b.png"
+  },
+  {
+    "revision": "a7b0d25e33ac1ae116e0fd36eab8f72a",
+    "url": "//Portfolio/static/media/pic.a7b0d25e.png"
+  }
+]);
